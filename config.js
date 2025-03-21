d081aeac69855bb8aa14546b9baa94a284925d4e
lee.gh <lee.gh@ensecure.co.kr>
2025-03-21 15:14:47 +0900

    initial commit

---
+++ config.js
@@ -0,0 +1,3 @@
+// configList = ["img", "name", "ability", "duration", "healing", "weight", "stack_size", "biome", "station"]
+// configList = ["img", "name", "type", "ability", "recipe", "duration", "biome"]
+configList = ["img", "name", "type", "health", "stamina", "eitr", "recipe", "duration", "biome"]
---
+++ recipe.js
@@ -0,0 +1,717 @@
+recipeList = [
+	{
+		"food_id" : 1,
+		"ingr" : "넥꼬리",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 2,
+		"ingr" : "멧돼지고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 3,
+		"ingr" : "사슴고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 4,
+		"ingr" : "멧돼지고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 4,
+		"ingr" : "꿀",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 5,
+		"ingr" : "멧돼지고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 5,
+		"ingr" : "넥꼬리",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 5,
+		"ingr" : "당근",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 6,
+		"ingr" : "라즈베리",
+		"qtt" : 8
+	},
+	{
+		"food_id" : 6,
+		"ingr" : "블루베리",
+		"qtt" : 6
+	},
+	{
+		"food_id" : 7,
+		"ingr" : "버섯",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 7,
+		"ingr" : "당근",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 8,
+		"ingr" : "블루베리",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 8,
+		"ingr" : "당근",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 8,
+		"ingr" : "요리한 사슴고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 9,
+		"ingr" : "날생선",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 10,
+		"ingr" : "수액",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 10,
+		"ingr" : "라즈베리",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 10,
+		"ingr" : "블루베리",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 11,
+		"ingr" : "멧돼지고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 11,
+		"ingr" : "순무",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 12,
+		"ingr" : "내장",
+		"qtt" : 4
+	},
+	{
+		"food_id" : 12,
+		"ingr" : "멧돼지고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 12,
+		"ingr" : "엉겅퀴",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 13,
+		"ingr" : "피주머니",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 13,
+		"ingr" : "꿀",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 13,
+		"ingr" : "순무",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 14,
+		"ingr" : "요리한 사슴고기",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 14,
+		"ingr" : "요리한 멧돼지고기",
+		"qtt" : 5
+	},
+	{
+		"food_id" : 14,
+		"ingr" : "민들레",
+		"qtt" : 4
+	},
+	{
+		"food_id" : 14,
+		"ingr" : "Woodland herb blend",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 15,
+		"ingr" : "사슴스튜",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 15,
+		"ingr" : "엉겅퀴",
+		"qtt" : 5
+	},
+	{
+		"food_id" : 15,
+		"ingr" : "Queen's jam",
+		"qtt" : 4
+	},
+	{
+		"food_id" : 15,
+		"ingr" : "Woodland herb blend",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 16,
+		"ingr" : "Sausages",
+		"qtt" : 8
+	},
+	{
+		"food_id" : 16,
+		"ingr" : "피주머니",
+		"qtt" : 4
+	},
+	{
+		"food_id" : 16,
+		"ingr" : "순무 stew",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 16,
+		"ingr" : "Woodland herb blend",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 17,
+		"ingr" : "바다뱀고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 18,
+		"ingr" : "버섯",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 18,
+		"ingr" : "요리한 바다뱀고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 18,
+		"ingr" : "꿀",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 19,
+		"ingr" : "구운 생선",
+		"qtt" : 5
+	},
+	{
+		"food_id" : 19,
+		"ingr" : "엉겅퀴",
+		"qtt" : 4
+	},
+	{
+		"food_id" : 19,
+		"ingr" : "요리한 바다뱀고기",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 19,
+		"ingr" : "Seafarer's herbs",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 20,
+		"ingr" : "양파",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 21,
+		"ingr" : "늑대고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 22,
+		"ingr" : "늑대고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 22,
+		"ingr" : "꿀",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 23,
+		"ingr" : "늑대고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 23,
+		"ingr" : "버섯",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 23,
+		"ingr" : "양파",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 24,
+		"ingr" : "회색난쟁이눈",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 24,
+		"ingr" : "동결선",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 25,
+		"ingr" : "늑대꼬치",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 25,
+		"ingr" : "양파 soup",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 25,
+		"ingr" : "당근",
+		"qtt" : 4
+	},
+	{
+		"food_id" : 25,
+		"ingr" : "Mountain peak pepper powder",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 26,
+		"ingr" : "록스고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 27,
+		"ingr" : "구운 생선",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 27,
+		"ingr" : "곡분",
+		"qtt" : 4
+	},
+	{
+		"food_id" : 28,
+		"ingr" : "곡분",
+		"qtt" : 4
+	},
+	{
+		"food_id" : 28,
+		"ingr" : "클라우드베리",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 28,
+		"ingr" : "록스고기",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 29,
+		"ingr" : "엉겅퀴",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 29,
+		"ingr" : "피주머니",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 29,
+		"ingr" : "곡분",
+		"qtt" : 4
+	},
+	{
+		"food_id" : 30,
+		"ingr" : "빵반죽",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 31,
+		"ingr" : "빵",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 31,
+		"ingr" : "록스고기 pie",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 31,
+		"ingr" : "클라우드베리",
+		"qtt" : 5
+	},
+	{
+		"food_id" : 31,
+		"ingr" : "Grasslands herbalist harvest",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 32,
+		"ingr" : "달걀",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 33,
+		"ingr" : "추적자고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 34,
+		"ingr" : "토끼고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 35,
+		"ingr" : "닭고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 36,
+		"ingr" : "추적자고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 36,
+		"ingr" : "록스고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 36,
+		"ingr" : "토끼고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 37,
+		"ingr" : "닭고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 37,
+		"ingr" : "꿀",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 37,
+		"ingr" : "요툰거품",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 38,
+		"ingr" : "토끼고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 38,
+		"ingr" : "요툰거품",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 38,
+		"ingr" : "당근",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 39,
+		"ingr" : "요툰거품",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 39,
+		"ingr" : "양파",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 39,
+		"ingr" : "클라우드베리",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 40,
+		"ingr" : "달걀",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 40,
+		"ingr" : "요툰거품",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 41,
+		"ingr" : "아귀",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 41,
+		"ingr" : "빵반죽",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 42,
+		"ingr" : "마법사갓",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 42,
+		"ingr" : "고형피",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 42,
+		"ingr" : "순무",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 43,
+		"ingr" : "이그드라실수액",
+		"qtt" : 4
+	},
+	{
+		"food_id" : 43,
+		"ingr" : "보리",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 43,
+		"ingr" : "로열젤리",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 44,
+		"ingr" : "추적자고기",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 44,
+		"ingr" : "마법사갓",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 44,
+		"ingr" : "로열젤리",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 45,
+		"ingr" : "안개토끼슈프림",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 45,
+		"ingr" : "요리한 추적자고기",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 45,
+		"ingr" : "이그드라실죽",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 45,
+		"ingr" : "Herbs of the hidden hills",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 46,
+		"ingr" : "애스크스빈꼬리",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 47,
+		"ingr" : "화산매고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 48,
+		"ingr" : "뼈주둥이고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 49,
+		"ingr" : "애스크스빈꼬리",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 49,
+		"ingr" : "넝쿨베리",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 49,
+		"ingr" : "연기거품",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 50,
+		"ingr" : "애스크스빈꼬리",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 50,
+		"ingr" : "화산매고기",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 50,
+		"ingr" : "고비",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 51,
+		"ingr" : "애스크스빈꼬리",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 51,
+		"ingr" : "넝쿨베리",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 51,
+		"ingr" : "곡분",
+		"qtt" : 4
+	},
+	{
+		"food_id" : 52,
+		"ingr" : "넝쿨베리",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 52,
+		"ingr" : "꿀",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 52,
+		"ingr" : "고비",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 53,
+		"ingr" : "요툰거품",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 53,
+		"ingr" : "양파",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 53,
+		"ingr" : "고비",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 54,
+		"ingr" : "화산매알",
+		"qtt" : 1
+	},
+	{
+		"food_id" : 54,
+		"ingr" : "넝쿨베리",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 54,
+		"ingr" : "곡분",
+		"qtt" : 4
+	},
+	{
+		"food_id" : 55,
+		"ingr" : "이그드라실수액",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 55,
+		"ingr" : "넝쿨베리",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 55,
+		"ingr" : "고비",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 56,
+		"ingr" : "이그드라실수액",
+		"qtt" : 4
+	},
+	{
+		"food_id" : 56,
+		"ingr" : "마법사갓",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 56,
+		"ingr" : "넝쿨베리",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 56,
+		"ingr" : "연기거품",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 57,
+		"ingr" : "이그드라실수액",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 57,
+		"ingr" : "마법사갓",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 57,
+		"ingr" : "고비",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 57,
+		"ingr" : "연기거품",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 58,
+		"ingr" : "요리한 애스크스빈꼬리",
+		"qtt" : 3
+	},
+	{
+		"food_id" : 58,
+		"ingr" : "넝쿨베리",
+		"qtt" : 5
+	},
+	{
+		"food_id" : 58,
+		"ingr" : "Scorching medley",
+		"qtt" : 2
+	},
+	{
+		"food_id" : 58,
+		"ingr" : "Fiery spice powder",
+		"qtt" : 1
+	}
+]
