148ab32bdbc341e6021b54933b9c0dd38b1bc956
gh <lljjll1235@gmail.com>
2025-03-23 00:25:01 +0900

    북마크 추가

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // configList = ["img", "name", "ability", "duration", "healing", "weight", "stack_size", "biome", "station"]
 // configList = ["img", "name", "type", "ability", "recipe", "duration", "biome"]
-configList = ["img", "name", "type", "health", "stamina", "eitr", "recipe", "duration", "biome"]
+configList = ["chk", "img", "name", "type", "health", "stamina", "eitr", "recipe", "duration", "biome"]
